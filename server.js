984dd2d15583a50835f1d78eb27ee3b0103c5f72
Varun Joseph <63628481+varun-joseph@users.noreply.github.com>
2022-11-21 19:35:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,29 @@ const app = express()
 const db = require('./db')
 const pizzasRoute = require('./routes/pizzasRoute')
 const userRoute = require('./routes/userRoute')
+const ordersRoute = require('./routes/ordersRoute')
 
-app.use(express.json())
-const port = process.env.PORT || 5000
+app.use('/api/pizzas/', pizzasRoute)
+app.use('/api/users/' , userRoute)
+app.use('/api/orders/' , ordersRoute)
 
-app.use('/api/pizzas', pizzasRoute)
-app.use('/api/users', userRoute)
-app.get('/', (req, res) => res.send('welcome to pizza app..!'))
 
-app.listen(port, () => console.log(`Example app listening on port port! ${port}`))
+if(process.env.NODE_ENV ==='production')
+{
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+
+    })
+}
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on port port `)
